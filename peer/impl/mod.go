ebee4374dda9fb24880e882d905111d756d1bd7f
wanhaozhou <wanhao.zhou@gmail.com>
2021-10-06 00:19:30 +0200

    HW-0 Implement basic chat functions based on channel

--- peer/impl/mod.go
+++ peer/impl/mod.go
@@ -2,9 +2,10 @@ package impl
 
 import (
 	"errors"
-	"fmt"
+	"github.com/rs/zerolog/log"
 	"go.dedis.ch/cs438/peer"
 	"go.dedis.ch/cs438/transport"
+	"go.dedis.ch/cs438/types"
 	"golang.org/x/xerrors"
 	"sync"
 	"time"
@@ -18,12 +19,14 @@ func NewPeer(conf peer.Configuration) peer.Peer {
 	newNode := &node{
 		config: conf,
 		address: conf.Socket.GetAddress(),
-		stop: false,
+		stop: make(chan bool),
 		routingTable: make(map[string]string),
 	}
 
 	// initialize routingTable
 	newNode.AddPeer(conf.Socket.GetAddress())
+	// Task 4: Implement a chat messaging mechanism
+	newNode.config.MessageRegistry.RegisterMessageCallback(types.ChatMessage{}, ExecChatMessage)
 	return newNode
 }
 
@@ -34,7 +37,7 @@ type node struct {
 	peer.Peer
 	config peer.Configuration
 	address string
-	stop bool
+	stop chan bool
 	routingTable peer.RoutingTable
 	routingTableMutex sync.RWMutex
 }
@@ -43,22 +46,26 @@ type node struct {
 func (n *node) Start() error {
 	go func() {
 		for {
-			// check if the node receives a stop signal
-			if n.stop {
-				return
-			}
+			select {
+				case <- n.stop:
+					close(n.stop)
+					return
 
-			pkt, err := n.config.Socket.Recv(time.Second * 1)
-			if errors.Is(err, transport.TimeoutErr(0)) {
-				continue
-			}
+				default:
+					pkt, err := n.config.Socket.Recv(time.Second * 1)
+					if errors.Is(err, transport.TimeoutErr(0)) {
+						continue
+					}
 
-			// TODO: handle pkt
-			if pkt.Header.Destination == n.address {
-				fmt.Println("received packet: ", pkt)
-				n.config.MessageRegistry.ProcessPacket(pkt)
-			} else {
-				fmt.Println("received packet of others: ", pkt)
+					if pkt.Header.Destination == n.address {
+						err = n.ProcessPacket(pkt)
+					} else {
+						err = n.RelayPacket(pkt)
+					}
+
+					if err != nil {
+						log.Error().Err(err)
+					}
 			}
 		}
 	}()
@@ -67,7 +74,7 @@ func (n *node) Start() error {
 
 // Stop implements peer.Service
 func (n *node) Stop() error {
-	n.stop = true
+	n.stop <- true
 	return nil
 }
 
@@ -78,15 +85,16 @@ func (n *node) Unicast(dest string, msg transport.Message) error {
 	defer n.routingTableMutex.RUnlock()
 
 	if _, present := n.routingTable[dest]; !present {
-		return xerrors.Errorf("cannot find destination: %v", dest)
+		return xerrors.Errorf("Cannot find destination: %v", dest)
 	}
 
+	// TODO: check the relay address
 	header := transport.NewHeader(n.address, n.address, dest, 0)
 	packet := transport.Packet{
 		Header: &header,
 		Msg: &msg,
 	}
-	err := n.config.Socket.Send(dest, packet, 0)
+	err := n.config.Socket.Send(n.routingTable[dest], packet, 0)
 	if err != nil {
 		// TODO: log the error
 		return err
@@ -129,4 +137,40 @@ func (n *node) SetRoutingEntry(origin, relayAddr string) {
 
 	// TODO: add a new neighbour
 	n.routingTable[origin] = relayAddr
+}
+
+
+func (n *node) ProcessPacket(pkt transport.Packet) error {
+	log.Info().Msgf(
+		"Receive packet from: %v, relay: %v, to: %v",
+		pkt.Header.Source,
+		pkt.Header.RelayedBy,
+		n.address)
+	return n.config.MessageRegistry.ProcessPacket(pkt)
+}
+
+func (n *node) RelayPacket(pkt transport.Packet) error {
+	n.routingTableMutex.RLock()
+	defer n.routingTableMutex.RUnlock()
+	if _, present := n.routingTable[pkt.Header.Destination]; !present {
+		return xerrors.Errorf("Cannot relay the message: from %v to %v", n.address, pkt.Header.Destination)
+	}
+	newPktHeader := transport.NewHeader(pkt.Header.Source, n.address, pkt.Header.Destination, pkt.Header.TTL - 1)
+	newPktMsg := pkt.Msg.Copy()
+	return n.config.Socket.Send(
+		n.routingTable[pkt.Header.Destination],
+		transport.Packet{
+			Header: &newPktHeader,
+			Msg: &newPktMsg,
+		},
+		0)
+}
+
+func ExecChatMessage(msg types.Message, pkt transport.Packet) error {
+	chatMsg, ok := msg.(*types.ChatMessage)
+	if !ok {
+		return xerrors.Errorf("Wrong type: %T", msg)
+	}
+	log.Info().Msgf("ExecChatMessage: receive chat message: %v", chatMsg)
+	return nil
 }
